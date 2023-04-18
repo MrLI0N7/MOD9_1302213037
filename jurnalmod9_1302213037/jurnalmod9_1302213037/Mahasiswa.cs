f42f0d04c6e50771c06b3a218cc1e3fe5b4caf8f
MrLI0N7 <93116969+MrLI0N7@users.noreply.github.com>
2023-04-18 16:41:24 +0800

    Initital Commit & penambahan kelas mahasiswa

---
+++ jurnalmod9_1302213037/jurnalmod9_1302213037/Mahasiswa.cs
@@ -0,0 +1,18 @@
+﻿namespace jurnalmod9_1302213037
+{
+    public class Mahasiswa
+    {
+        public string nama { get; set; }
+        public string nim { get; set;}
+        public List<string> Course { get; set;}
+        public int year { get; set;}
+
+        public Mahasiswa(string nama, string nim, List<string> Course, int year)
+        {
+            this.nama = nama; 
+            this.nim = nim; 
+            this.Course = Course; 
+            this.year = year;
+        }
+    }
+}
